d52196f27c36d6c25d6ce2f610f24260ab6b34f5
Eapen Mani <eapenmani@gmail.com>
2023-03-15 11:43:36 +0530

    Added script for table and data creation

---
+++ backend-flask/db/seed.sql
@@ -0,0 +1,13 @@
+-- this file was manually created
+INSERT INTO public.users (display_name, handle, cognito_user_id)
+VALUES
+  ('Eapen Mani', 'eapenm' ,'MOCK'),
+  ('Bin Mani', 'binmani' ,'MOCK');
+
+INSERT INTO public.activities (user_uuid, message, expires_at)
+VALUES
+  (
+    (SELECT uuid from public.users WHERE users.handle = 'eapenm' LIMIT 1),
+    'This was imported as seed data!',
+    current_timestamp + interval '10 day'
+  )

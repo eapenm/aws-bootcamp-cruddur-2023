28ead5145719f97a84fdaf438a109dfdf8e8cd3a
eapenm <eapenmani@gmail.com>
2023-03-17 02:08:07 +0000

    Query working

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -5,18 +5,18 @@ connection_url = os.getenv("CONNECTION_URL")
 pool = ConnectionPool(connection_url)
 
 def query_wrap_object(template):
-  sql = f'''
+  sql = f"""
   (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
   {template}
   ) object_row);
-  '''
+  """
   return sql
 
 def query_wrap_array(template):
-  sql = f'''
+  sql = f"""
   (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
   {template}
   ) array_row);
-  '''
+  """
   return sql
 

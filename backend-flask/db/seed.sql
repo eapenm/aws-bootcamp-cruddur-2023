960f7b17a8c9168c02844633d9798310a1e08670
Eapen Mani <eapenmani@gmail.com>
2023-03-24 06:48:54 +0530

    db scripts added for user creation

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Eapen Mani', 'eapenm' ,'MOCK'),
-  ('Bin Mani', 'robinm' ,'MOCK');
+  ('Eapen Mani','eapenmani@yahoo.com' , 'eapenm' ,'MOCK'),
+  ('Robin Mani','eapenmani@gmail.com' , 'robinm' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES

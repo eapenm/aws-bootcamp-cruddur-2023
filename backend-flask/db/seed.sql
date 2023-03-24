9e8b438b901099bb276ee3bc244194351b54bc13
Eapen Mani <eapenmani@gmail.com>
2023-03-24 06:00:32 +0530

    Added dynamo related scripts

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,7 +2,7 @@
 INSERT INTO public.users (display_name, handle, cognito_user_id)
 VALUES
   ('Eapen Mani', 'eapenm' ,'MOCK'),
-  ('Bin Mani', 'binmani' ,'MOCK');
+  ('Bin Mani', 'robinm' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES

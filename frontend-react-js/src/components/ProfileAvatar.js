cb56ac7277e2b35f9970674827413dddb919093b
eapenm <eapenmani@gmail.com>
2023-05-21 02:17:14 +0000

    Updated the pages

--- frontend-react-js/src/components/ProfileAvatar.js
+++ frontend-react-js/src/components/ProfileAvatar.js
@@ -1,7 +1,8 @@
 import './ProfileAvatar.css';
 
 export default function ProfileAvatar(props) {
-  const backgroundImage = `url("https://assets.eapencloud.link/avatars/${props.id}.jpg")`;
+  //const backgroundImage = `url("https://assets.eapencloud.link/avatars/${props.id}.jpg")`;
+  const backgroundImage = `url("https://assets.eapencloud.link/avatars/data.jpg")`;
   const styles = {
     backgroundImage: backgroundImage,
     backgroundSize: 'cover',
